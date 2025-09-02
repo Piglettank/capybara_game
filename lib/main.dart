5652ad8552769d62eacdef434a71f12cff94986d
ylruiz <yunet.lruiz94@gmail.com>
2025-09-02 12:40:21 +0200

    add obstacle

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:capybara_game/ground.dart';
+import 'package:capybara_game/obstacle.dart';
 import 'package:capybara_game/player.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
@@ -14,14 +15,17 @@ class CapybaraGame extends FlameGame
     with HasKeyboardHandlerComponents, HasCollisionDetection {
   final Player player = Player();
   final Ground ground = Ground();
+  final Obstacle obstacle = Obstacle();
   final double gravity = 100;
 
   @override
   FutureOr<void> onLoad() {
     player.position = Vector2(100, size.y * 0.70);
     ground.position = Vector2(0, size.y * 0.80);
+    obstacle.position = Vector2(400, size.y * 0.70);
     debugMode = true;
     add(player);
     add(ground);
+    add(obstacle);
   }
 }
